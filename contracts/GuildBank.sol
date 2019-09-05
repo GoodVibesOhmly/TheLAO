7b7ef844486ad0e5fbac42d4f75cb6f9570934f2
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2019-09-04 20:28:39 -0400

    Update GuildBank.sol
    clean up formatting changes

--- contracts/GuildBank.sol
+++ contracts/GuildBank.sol
@@ -13,26 +13,26 @@ contract GuildBank is Ownable {
 	
 	// contributionToken is used to fund ventures and distribute dividends, e.g., wETH or DAI
 	constructor(address contributionTokenAddress) public {
-    	contributionToken = IERC20(contributionTokenAddress);
+    		contributionToken = IERC20(contributionTokenAddress);
 	}
 
-    // pairs to VentureMoloch member ragequit mechanism
+    	// pairs to VentureMoloch member ragequit mechanism
 	function withdraw(address receiver, uint256 amount) public onlyOwner returns (bool) {
-    	emit Withdrawal(receiver, amount);
-    	return contributionToken.transfer(receiver, amount);
+    		emit Withdrawal(receiver, amount);
+    		return contributionToken.transfer(receiver, amount);
 	}
 	
 	// pairs to VentureMoloch member dividend claiming mechanism
 	function withdrawDividend(address receiver, uint256 amount) public onlyOwner returns (bool) {
-    	emit Withdrawal(receiver, amount);
-    	return contributionToken.transfer(receiver, amount);
+    		emit Withdrawal(receiver, amount);
+    		return contributionToken.transfer(receiver, amount);
 	}
     
-    // pairs to VentureMoloch funding proposal mechanism. 
-    // Funds are withdrawn on processProposal
+        // pairs to VentureMoloch funding proposal mechanism. 
+        // Funds are withdrawn on processProposal
 	function withdrawFunds(address applicant, uint256 fundsRequested) public onlyOwner returns (bool) {
-    	emit FundsWithdrawal(applicant, fundsRequested);
-    	return contributionToken.transfer(applicant, fundsRequested);
+    		emit FundsWithdrawal(applicant, fundsRequested);
+    		return contributionToken.transfer(applicant, fundsRequested);
 	}
 	
 	// onlySummoner in Moloch can withdraw and administer investment tokens
